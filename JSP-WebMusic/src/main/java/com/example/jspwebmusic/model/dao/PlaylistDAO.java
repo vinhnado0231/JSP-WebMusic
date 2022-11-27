40e98844fd00ec86b124c05a4790f91f1073efc8
Huu Phuc <phucchp@gmail.com>
2022-11-27 15:22:33 +0700

    code playlistDAO

--- JSP-WebMusic/src/main/java/com/example/jspwebmusic/model/dao/PlaylistDAO.java
+++ JSP-WebMusic/src/main/java/com/example/jspwebmusic/model/dao/PlaylistDAO.java
@@ -1,4 +1,31 @@
 package com.example.jspwebmusic.model.dao;
 
+import com.example.jspwebmusic.model.bean.Song;
+
+import java.sql.*;
+import java.util.ArrayList;
+
 public class PlaylistDAO {
+    public PlaylistDAO(){}
+    public ArrayList<Song> getAllSong(){
+        ArrayList<Song> listSongs=new ArrayList<>();
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            String url = "jdbc:mysql://localhost:3306/musicdb";
+            Connection conn = DriverManager.getConnection(url, "root", "");
+            Statement stmt = conn.createStatement();
+            String sql = "select * from song";
+            ResultSet rs=stmt.executeQuery(sql);
+            while(rs.next()){
+                Song baihat=new Song();
+            }
+        } catch (ClassNotFoundException e) {
+            throw new RuntimeException(e);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+
+
+        return listSongs;
+    }
 }
