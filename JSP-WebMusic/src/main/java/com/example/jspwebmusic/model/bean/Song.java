9e7da8b7dadbbde52d2d0d896a770fdd6e9e30f0
vinhnado0231 <vinh.nd0231@gmail.com>
2022-11-27 15:21:10 +0700

    add file

--- JSP-WebMusic/src/main/java/com/example/jspwebmusic/model/bean/Song.java
+++ JSP-WebMusic/src/main/java/com/example/jspwebmusic/model/bean/Song.java
@@ -1,7 +1,89 @@
 package com.example.jspwebmusic.model.bean;
 
 public class Song {
-    private long id;
+    private long idSong;
     private String tenBaiHat;
-    private String
+    private String caSi;
+    private String loiBaiHat;
+    private String sangTac;
+    private String thoiGian;
+    private long luotNghe;
+    private String target;
+
+    public long getIdSong() {
+        return idSong;
+    }
+
+    public void setIdSong(long idSong) {
+        this.idSong = idSong;
+    }
+
+    public String getTenBaiHat() {
+        return tenBaiHat;
+    }
+
+    public void setTenBaiHat(String tenBaiHat) {
+        this.tenBaiHat = tenBaiHat;
+    }
+
+    public String getCaSi() {
+        return caSi;
+    }
+
+    public void setCaSi(String caSi) {
+        this.caSi = caSi;
+    }
+
+    public String getLoiBaiHat() {
+        return loiBaiHat;
+    }
+
+    public void setLoiBaiHat(String loiBaiHat) {
+        this.loiBaiHat = loiBaiHat;
+    }
+
+    public String getSangTac() {
+        return sangTac;
+    }
+
+    public void setSangTac(String sangTac) {
+        this.sangTac = sangTac;
+    }
+
+    public String getThoiGian() {
+        return thoiGian;
+    }
+
+    public void setThoiGian(String thoiGian) {
+        this.thoiGian = thoiGian;
+    }
+
+    public long getLuotNghe() {
+        return luotNghe;
+    }
+
+    public void setLuotNghe(long luotNghe) {
+        this.luotNghe = luotNghe;
+    }
+
+    public String getTarget() {
+        return target;
+    }
+
+    public void setTarget(String target) {
+        this.target = target;
+    }
+
+    public Song(String tenBaiHat, String caSi, String loiBaiHat, String sangTac, String thoiGian, long luotNghe, String target) {
+        this.tenBaiHat = tenBaiHat;
+        this.caSi = caSi;
+        this.loiBaiHat = loiBaiHat;
+        this.sangTac = sangTac;
+        this.thoiGian = thoiGian;
+        this.luotNghe = luotNghe;
+        this.target = target;
+    }
+
+    public Song() {
+    }
 }
