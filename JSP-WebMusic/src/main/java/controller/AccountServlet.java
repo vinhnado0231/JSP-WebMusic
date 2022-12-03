496fb4cf1d97ecf30ae10f61f5c2f0908120011a
vinhnado0231 <vinh.nd0231@gmail.com>
2022-12-03 16:05:27 +0700

    OK

--- JSP-WebMusic/src/main/java/controller/AccountServlet.java
+++ JSP-WebMusic/src/main/java/controller/AccountServlet.java
@@ -4,9 +4,13 @@ import model.bean.User;
 import model.bo.AccountBO;
 import model.bo.UserBO;
 
-import javax.servlet.*;
-import javax.servlet.http.*;
-import javax.servlet.annotation.*;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import java.io.IOException;
 
 @WebServlet(name = "AccountServlet", value = "/AccountServlet")
@@ -15,43 +19,84 @@ public class AccountServlet extends HttpServlet {
     private UserBO userBO = new UserBO();
 
     @Override
-    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        doPost(request, response);
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)  {
+        HttpSession session = request.getSession();
+        String action = request.getParameter("action");
+        if (action == null) {
+            action = "";
+        }
+        try {
+            switch (action) {
+                case "loginForm":
+                    displayLoginForm(request, response);
+                    break;
+                case "logout":
+                    logout(request, response);
+                    break;
+                case "delete":
+                    break;
+                case "search":
+                    break;
+                default:
+                    session.setAttribute("changePage", "home");
+                    response.sendRedirect("/");
+                    break;
+            }
+        } catch (ServletException | IOException ex) {
+            ex.printStackTrace();
+        }
     }
 
     @Override
-    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException{
         HttpSession session = request.getSession();
+        String action = request.getParameter("action");
+        if (action == null) {
+            action = "";
+        }
+        try {
+            switch (action) {
+                case "login":
+                    checkLogin(request, response);
+                    break;
+                case "delete":
+                    break;
+                case "search":
+                    break;
+                default:
+                    session.setAttribute("changePage", "home");
+                    response.sendRedirect("/");
+                    break;
+            }
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+    }
 
+    private void displayLoginForm(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        RequestDispatcher rd = getServletContext().getRequestDispatcher("/login-form.jsp");
+        rd.forward(request, response);
+    }
+
+    private void logout(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        HttpSession session = request.getSession();
+        session.setAttribute("checkLogin", false);
         session.setAttribute("changePage", "home");
+        response.sendRedirect("/");
+    }
 
-        if (request.getParameter("action").equals("loginForm")) {
+    private void checkLogin(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        HttpSession session = request.getSession();
+        if (accountBO.checkLogin(request.getParameter("username"), request.getParameter("password"))) {
+            session.setAttribute("checkLogin", true);
+            User userNow = userBO.getUserByIdaccount(accountBO.findAccountByUsername(request.getParameter("username")).getIdaccount());
+            session.setAttribute("userNow", userNow);
+            session.setAttribute("changePage", "home");
+            response.sendRedirect("/");
+        } else {
+            session.setAttribute("checkLogin", false);
             RequestDispatcher rd = getServletContext().getRequestDispatcher("/login-form.jsp");
             rd.forward(request, response);
-        } else if (request.getParameter("action").equals("login")) {
-            if (accountBO.checkLogin(request.getParameter("username"), request.getParameter("password"))) {
-                session.setAttribute("checkLogin", true);
-                User userNow =  userBO.getUserByIdaccount(accountBO.findAccountByUsername(request.getParameter("username")).getIdaccount());
-                session.setAttribute("userNow", userNow);
-                
-                session.setAttribute("changePage", "home");
-
-                response.sendRedirect("/");
-            } else {
-                session.setAttribute("checkLogin", false);
-                RequestDispatcher rd = getServletContext().getRequestDispatcher("/login-form.jsp");
-                rd.forward(request, response);
-            }
-        } else if (request.getParameter("action").equals("logout")) {
-            session.setAttribute("checkLogin", false);
-
-            session.setAttribute("changePage", "home");
-
-            response.sendRedirect("/");
-        } else if (request.getParameter("action").equals("/")) {
-            session.setAttribute("changePage", "home");
-            response.sendRedirect("/");
         }
     }
 }
